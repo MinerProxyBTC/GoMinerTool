e6c8753a0165acae19646fe2de871736d9c086bc
GoMinerproxy <99313894+MinerProxyBTC@users.noreply.github.com>
2022-11-23 22:18:08 +0800

    重要更新!
    大幅度提升小币种稳定性提升,建议所有老版本更新！

--- linux-install.sh
+++ linux-install.sh
@@ -2,7 +2,7 @@
 # Author: MinerProxyBTC
 # github: https://github.com/MinerProxyBTC/GoMinerTool
 
-VERSION="2.8.9"
+VERSION="2.9.2"
 
 DOWNLOAD_HOST="https://github.com/MinerProxyBTC/GoMinerTool/raw/main/Linux-64"
 
@@ -41,6 +41,7 @@ ISSUE() {
     echo "2.8.6"
     echo "2.8.8"
     echo "2.8.9"
+    echo "2.9.2"
 }
 
 colorEcho(){
@@ -177,7 +178,7 @@ start() {
 update() {
     turn_off
 
-    installapp 2.8.9
+    installapp 2.9.2
 }
 
 turn_on() {
@@ -447,7 +448,7 @@ read -p "$(echo -e "请选择[1-19]：")" choose
 
 case $choose in
 1)
-    installapp 2.8.9
+    installapp 2.9.2
     ;;
 2)
     update
